f2fd7d32cd12656af5723bb7035c09f83e623419
William Hinthorn <hinthornw@nat-oitwireless-inside-vapornet100-d-12404.Princeton.EDU>
2017-12-08 19:32:40 -0500

    .

--- hit_templates/js/get-obj.js
+++ hit_templates/js/get-obj.js
@@ -4,7 +4,7 @@ var VG = (function(vg, $) {
         AWS.config.update({accessKeyId: 'AKIAIGY2E2BJRDZEKAHQ', secretAccessKey: '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', region: 'us-east-1'});
         var buck = 'visualaipascalparts';
         var s3Client = new AWS.S3();
-        console.log('Getting: ' +  key + ' from ' buck);
+        console.log('Getting: ' +  key + ' from ' + buck);
         var params = {
             Bucket: buck,
             Key: key
