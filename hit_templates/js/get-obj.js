0f9ea5262c800223caee49f3b3739de13ad23e40
William Hinthorn <hinthornw@nat-oitwireless-inside-vapornet100-d-12404.Princeton.EDU>
2017-12-08 20:22:27 -0500

    .

--- hit_templates/js/get-obj.js
+++ hit_templates/js/get-obj.js
@@ -2,7 +2,7 @@ var VG = (function(vg, $) {
     VG.getObj = function(ind, key, cb) {
         var that = (this === vg ? {} : this);
         //AWS.config.update({accessKeyId: 'AKIAIGY2E2BJRDZEKAHQ', secretAccessKey: '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', region: 'us-east-1'});
-        AWS.config.update({accessKeyId: 'AKIAJUCW44V64NZPWXMA', secretAccessKey: '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', region: 'us-east-1'});
+        AWS.config.update({accessKeyId: 'AKIAJYX5KHE6LKNNXKCQ', secretAccessKey: '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', region: 'us-east-1'});
         var buck = 'visualaipascalparts';
         var s3Client = new AWS.S3();
         var params = {
--- hit_templates/js/put-obj-s3.js
+++ hit_templates/js/put-obj-s3.js
@@ -6,7 +6,8 @@ var VG = (function(vg, $) {
         var uploadFileName = 'points_task_'+resultsTime + '.json'
 
         //AWS.config.update({accessKeyId: 'AKIAIGY2E2BJRDZEKAHQ', secretAccessKey: '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', region: 'us-east-1'});
-        AWS.config.update({accessKeyId: 'AKIAJUCW44V64NZPWXMA', secretAccessKey: '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', region: 'us-east-1'});
+        //AWS.config.update({accessKeyId: 'AKIAJUCW44V64NZPWXMA', secretAccessKey: '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', region: 'us-east-1'});
+        AWS.config.update({accessKeyId: 'AKIAJYX5KHE6LKNNXKCQ', secretAccessKey: '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', region: 'us-east-1'});
         var buck = 'visualaipascalresponses';
         var s3Client = new AWS.S3();
         var params = {
