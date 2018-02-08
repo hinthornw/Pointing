d7c08d22dcb75cffba4e4703015d89722190ea62
William Hinthorn <hinthornw@WilliamdeMacBook-Pro.local>
2018-02-08 11:32:49 -0500

    Red circle

--- hit_templates/js/drawpoint.js
+++ hit_templates/js/drawpoint.js
@@ -2,14 +2,15 @@ var VG = (function(vg) {
 
     vg.drawPoint= function(ctx, point) {
         ctx.fillStyle = "#ff2626"; // Red color
-        var pointSize = 4;
+        var pointSize = 3.5;
 
         ctx.beginPath();
         ctx.arc(point[1], point[0], pointSize, 0, Math.PI * 2, true);
         ctx.fill();
 
         //Surround in a ring
-        ctx.strokeStyle = '#42f44b';
+        //ctx.strokeStyle = '#42f44b';
+        ctx.strokeStyle =  "#ff2626"; 
         var circleSize = 10;
         ctx.arc(point[1], point[0], circleSize, 0, 2* Math.PI);
         ctx.stroke()
