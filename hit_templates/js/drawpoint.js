65e25c89b1f85b4086c26a1f7cdc87bb708eae92
William Hinthorn <hinthornw@nat-oitwireless-inside-vapornet100-c-30333.Princeton.EDU>
2018-02-12 10:31:12 -0500

    Centering

--- hit_templates/js/drawpoint.js
+++ hit_templates/js/drawpoint.js
@@ -1,23 +1,26 @@
 var VG = (function(vg) {
 
-    vg.drawPoint= function(ctx, point) {
+    vg.drawPoint= function(ctx, point, wStart, hStart) {
         ctx.fillStyle = "#ff2626"; // Red color
         var pointSize = 3.5;
+        
+        x = point[1]+wStart;
+        y = point[0]+hStart;
 
         ctx.beginPath();
-        ctx.arc(point[1], point[0], pointSize, 0, Math.PI * 2, true);
+        ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
         ctx.fill();
         // Fill in a ring
         ctx.strokeStyle = '#42f44b';
         var circleSize = 2;
-        ctx.arc(point[1], point[0], circleSize, 0, 2* Math.PI);
+        ctx.arc(x, y, circleSize, 0, 2* Math.PI);
         ctx.stroke()
 
         //Surround in a ring
         //ctx.strokeStyle = '#42f44b';
         ctx.strokeStyle =  "#ff2626"; 
         var circleSize = 10;
-        ctx.arc(point[1], point[0], circleSize, 0, 2* Math.PI);
+        ctx.arc(x, y, circleSize, 0, 2* Math.PI);
         ctx.stroke()
         //console.log("Drew point (" + point + ") ");
     }
