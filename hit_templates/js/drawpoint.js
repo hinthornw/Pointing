a5d2727e11ab9624dee6483802fd6240a5f82a85
William Hinthorn <hinthornw@nat-oitwireless-inside-vapornet100-c-30333.Princeton.EDU>
2018-02-13 20:06:01 -0500

    flexible point size

--- hit_templates/js/drawpoint.js
+++ hit_templates/js/drawpoint.js
@@ -1,8 +1,8 @@
 var VG = (function(vg) {
 
-    vg.drawPoint= function(ctx, point, wStart, hStart) {
+    vg.drawPoint= function(ctx, point, wStart, hStart, pointSize) {
         ctx.fillStyle = "#ff2626"; // Red color
-        var pointSize = 3.5;
+        //var pointSize = 3.5;
         
         x = point[1]+wStart;
         y = point[0]+hStart;
@@ -10,16 +10,17 @@ var VG = (function(vg) {
         ctx.beginPath();
         ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
         ctx.fill();
+
         // Fill in a ring
-        ctx.strokeStyle = '#42f44b';
-        var circleSize = 2;
-        ctx.arc(x, y, circleSize, 0, 2* Math.PI);
-        ctx.stroke()
+        //ctx.strokeStyle = '#42f44b';
+        //var circleSize = 2;
+        //ctx.arc(x, y, circleSize, 0, 2* Math.PI);
+        //ctx.stroke()
 
         //Surround in a ring
         //ctx.strokeStyle = '#42f44b';
         ctx.strokeStyle =  "#ff2626"; 
-        var circleSize = 10;
+        var circleSize = pointSize+pointSize*2;
         ctx.arc(x, y, circleSize, 0, 2* Math.PI);
         ctx.stroke()
         //console.log("Drew point (" + point + ") ");
